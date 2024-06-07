ddd8500a68bfdfdd55b0d9d1344fd90830242f0b
DominikaDuralek <dominikaduralek@gmail.com>
2024-06-07 09:34:41 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Algorithms and data structures");
+        System.out.println("Testing");
     }
 }
---
+++ src/datastructures/Stack.java
@@ -0,0 +1,7 @@
+package datastructures;
+
+public class Stack {
+    public static void main(String[] args){
+        System.out.println("Stack");
+    }
+}
